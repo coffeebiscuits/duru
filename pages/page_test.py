01b27b96eb04680b9ceba694216b9d040512ab5f
coffeebiscuits <opopkl-@nate.com>
2025-07-09 18:00:52 +0900

    Create page_test.py

---
+++ pages/page_test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import datetime
+from streamlit_autorefresh import st_autorefresh
+
+count = st_autorefresh(interval=1000, limit=None, key="auto_refresh")
+
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+st.write(f"현재 시간: {now}")
