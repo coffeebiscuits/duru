8f536e7709d3a29906cd541b050aa4028f8fdda6
coffeebiscuits <opopkl-@nate.com>
2025-07-09 16:53:38 +0900

    Update AX.py

--- llm/AX.py
+++ llm/AX.py
@@ -3,18 +3,11 @@ import requests
 from openai import OpenAI
 from dotenv import load_dotenv
 
-
-# 미스트랄 API 설정
 load_dotenv()
-Mistral_api_key = os.environ.get('Mistral_api_key')
-client = Mistral(api_key=Mistral_api_key)
-
-
-load_dotenv()
-api_key = os.environ.get('AX_api_key')
+api_key = os.environ.get('sktax_api_key')
 client = OpenAI(
     base_url="https://guest-api.sktax.chat/v1",
-    api_key=AX_api_key
+    api_key=sktax_api_key
 )
 
 def get_chat_response(user_input):
