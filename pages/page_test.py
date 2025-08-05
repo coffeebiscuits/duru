0701b78c0b29b5c62974211be35abefad3486474
coffeebiscuits <opopkl-@nate.com>
2025-08-05 17:28:02 +0900

    Update page_test.py

--- pages/page_test.py
+++ pages/page_test.py
@@ -6,3 +6,4 @@ count = st_autorefresh(interval=1000, limit=None, key="auto_refresh")
 
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 st.write(f"현재 시간: {now}")
+st.write("테스트 글자입니다.")
