bb4b122c47e2310723ac4e50a3389f5adcb81394
coffeebiscuits <opopkl-@nate.com>
2026-02-06 19:31:15 +0900

    Delete pages directory

--- pages/page_test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import datetime
-from streamlit_autorefresh import st_autorefresh
-
-count = st_autorefresh(interval=1000, limit=None, key="auto_refresh")
-
-now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-st.write(f"현재 시간: {now}")
-st.write("테스트 글자입니다.")
